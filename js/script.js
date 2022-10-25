bbd18725facf5256ce151a23b9485d4ecbfdb550
Leograzioli <leograzioli@gmail.com>
2022-10-25 14:22:07 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            message: "Ciao tutor come stai? :)"
+            message: "Ciao tutor come stai? :)",
+            srcImage: "img/6890001.jpg"
         }
     }, 
 }).mount('#app')
