ea03a169272217716a0d9139c10eb5d03b018e42
Leograzioli <leograzioli@gmail.com>
2022-10-25 14:47:19 +0200

    bonus inventato da me

--- js/script.js
+++ js/script.js
@@ -3,8 +3,24 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            message: "Ciao tutor come stai? :)",
-            srcImage: "img/6890001.jpg"
+            message: "Ciao come stai? :)",
+            person: "",
+            srcImage: "",
+            greet: ""
         }
     }, 
+    methods: {
+        witchTutor() {
+            if (this.person === "mauro") {
+                this.srcImage = "img/6890001.jpg"
+                this.greet = `welcome ${this.person}`
+            } else if (this.person === "marco"){
+                this.srcImage = "img/metallo-dado-d20-da-20-mm.jpg"
+                this.greet = `welcome ${this.person}`
+            } else if (this.person === "olga") {
+                this.srcImage = "img/shutterstock_1830294143.webp"
+                this.greet = `welcome ${this.person}`
+            } 
+        }
+    }
 }).mount('#app')
