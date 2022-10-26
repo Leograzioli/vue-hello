a80c6ddf4695040fb024aacf37b5166a5ed76836
Leograzioli <leograzioli@gmail.com>
2022-10-26 10:21:54 +0200

    sistemato indentazione

--- js/script.js
+++ js/script.js
@@ -16,16 +16,19 @@ createApp({
                 this.srcImage = "https://www.gameway.it/img/f/medium/6890001.jpg"
                 this.greet = `welcome ${this.person}`
                 this.altImage = "dice 20 face red"
+
             } else if (this.person === "marco"){
                 this.srcImage = "img/metallo-dado-d20-da-20-mm.jpg"
                 this.greet = `welcome ${this.person}`
                 this.altImage = "dice 20 face chrome"
+
             } else if (this.person === "olga") {
                 this.srcImage = "img/shutterstock_1830294143.webp"
                 this.greet = `welcome ${this.person}`
                 this.altImage = "white cat"
+
             } else {
-                this.greet = `please picka name`
+                this.greet = `please pick a name`
             }
         }
     }
